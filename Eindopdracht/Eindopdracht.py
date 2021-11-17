56381395a462c7bfc810b68d2bb4e563acda0791
Tom Oosterbaan <tom.oosterbaan@student.rocva.nl>
2021-11-17 23:07:06 +0100

    opdracht gemaakt

---
+++ Eindopdracht/Eindopdracht.py
@@ -0,0 +1,15 @@
+guess = input("why was 6 afraid of 7? ")
+
+if guess == "because 7 ate 9":
+    print("funny, right?")
+else:
+    print("Nope, That's not it!")
+
+print("Thanks for playing!")
+
+def smiley():
+    print(":)")
+
+smiley()
+smiley()
+smiley()
